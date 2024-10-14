f2aacbb7714391403609ebc82604b6ad1c297b19
Redmoon <139155210+tn00627974@users.noreply.github.com>
2024-10-14 11:27:38 +0800

    Update Guests.sql

--- MSSQL/temple_library/Guests.sql
+++ MSSQL/temple_library/Guests.sql
@@ -1,7 +1,7 @@
 
 -- Guests 資料表 
 CREATE TABLE Guests (
-Guest_ID CHAR(20) PRIMARY KEY NOT NULL, -- 出版商編碼
+Guest_ID CHAR(20) PRIMARY KEY, -- 出版商編碼
 name CHAR(20) NOT NULL ,  -- 出版商名稱
 sex CHAR(20), -- 聯絡人
 tel CHAR(20), -- 電話
