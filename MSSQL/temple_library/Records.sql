4a7dc7f245e7c89370194fb721aee711289fb67c
Redmoon <139155210+tn00627974@users.noreply.github.com>
2024-10-14 11:28:07 +0800

    Update Records.sql

--- MSSQL/temple_library/Records.sql
+++ MSSQL/temple_library/Records.sql
@@ -1,5 +1,5 @@
 CREATE TABLE Records (
-    Guest_ID CHAR(20) PRIMARY KEY NOT NULL, -- 出版商編碼
+    Guest_ID CHAR(20) PRIMARY KEY , -- 出版商編碼
     Book_ID INT NOT NULL,   
     date DATE,
     PRIMARY KEY (Guest_ID, Book_ID) -- 複合主鍵
