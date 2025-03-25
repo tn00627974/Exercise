28ba6f5027a28db83cb20332354c1341f23b90b7
Ulysses <tn00627974@gmail.com>
2025-03-25 23:24:20 +0800

    add EF connected

--- C#/Web_API/HelloWorld/HelloWorld/Program.cs
+++ C#/Web_API/HelloWorld/HelloWorld/Program.cs
@@ -1,3 +1,6 @@
+using HelloWorld.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,8 +9,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddDbContext<WebApiDataContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 var app = builder.Build();
+var dbContext = app.Services.CreateScope().ServiceProvider.GetRequiredService<WebApiDataContext>();
+dbContext.Database.OpenConnection();
+Console.WriteLine("資料庫連線成功");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
