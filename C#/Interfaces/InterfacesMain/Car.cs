c69d821fb019d14331302d30caf55555065f42e6
FirstCompany_小朱 <tn00627974@gmail.com>
2025-08-11 17:20:53 +0800

    study interface

---
+++ C#/Interfaces/InterfacesMain/Car.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace InterfacesMain
+{
+    public class Cars
+    {
+        public interface ICars
+        {
+            void Drive(string direction);
+        }
+
+        public class Toyota : ICars
+        {
+            public void Drive(string direction) { Console.WriteLine($"Car is driving{direction}"); }
+        }
+
+        public class Honda : ICars
+        {
+            public void Drive(string direction) { Console.WriteLine($"Car is driving{direction}"); }
+        }
+    }
+}
---
+++ C#/Interfaces/InterfacesMain/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using static InterfacesMain.Animal;
+using static InterfacesMain.Cars;
+
+namespace InterfacesMain
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat mini = new Cat();
+            mini.Eat("fish");
+            mini.MakeSound();
+
+            Dog chocho = new Dog();
+            chocho.Eat("cesar");
+            chocho.MakeSound();
+
+            Toyota rav4 = new Toyota();
+            rav4.Drive("Taipei");
+
+            Toyota fit = new Toyota();
+            rav4.Drive("Tainan");
+        }
+
+
+    }
+}
