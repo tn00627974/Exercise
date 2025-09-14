f35770d1c77cb9fdbe1ddb6a35da41e2c5b8be2a
DESKTOP-TM1N0UT\Ulysses <tn00627974@gmail.com>
2025-09-14 15:23:32 +0800

    Leam Action , Func , Lambda - Action 用於沒有回傳值的方法 - Func 用於有回傳值的方法 - Lambda 取代一次性匿名Funtion並簡寫與簡潔

--- C#/Delegate/DelegateExample/DelegateExample/Delegate1.cs
+++ C#/Delegate/DelegateExample/DelegateExample/Delegate1.cs
@@ -1,33 +1,106 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DelegateExample
+﻿namespace DelegateExample
 {
     internal class Delegate1
     {
-        public delegate void MyDelegate();
+        #region
+        public delegate void MyDelegate(); // 宣告委派
+        public MyDelegate _myDelegate; // 宣告委派變數
 
-        private static MyDelegate _myDelegate;
+        public delegate int MythDelegate(int a , int b); // 宣告委派
+        public MythDelegate _mythDelegate; // 宣告委派變數
+        #endregion
+
+        #region Action 用於沒有回傳值的方法 
+        public Action<int> _myAction1; // 用Action同時宣告 及 宣告委派變數
+        public Action<string> _myAction2; // 用Action同時宣告 及 宣告委派變數
+        #endregion
+
+        #region Func 用於有回傳值的方法 
+        public Func<int, int> _myAction3; // 用Func同時宣告 及 宣告委派變數 | <int,int> 第一個int是參數，第二個int是回傳值
+        public Func<string,string> _myAction4; // 用Func同時宣告 及 宣告委派變數 | <string,string> 第一個string是參數，第二個string是回傳值
+        #endregion
+
+        #region Lambda 表達式
+        public Action _myAction5;
+        public Func<int, int, int> _mythDelegate1;
+        #endregion
 
         public void Main()
         {
-            _myDelegate += SendMsg;
-            _myDelegate += SendMsg2;
+            // 多重委派
+            _myDelegate += SendMsg0;
             _myDelegate += PrintSum;
             _myDelegate();
+
+            // 移除方法
+            _myDelegate -= SendMsg0;
+            _myDelegate -= PrintSum;
+            //_myDelegate(); // **因為移除了方法，如果是空的這一行會報錯
+
+            // 判斷委派為空的寫法
+            if (_myDelegate != null) { _myDelegate(); } // 判斷是不是為空
+            _myDelegate?.Invoke(); // 為空更簡潔的寫法
+
+            // 帶入Action <int> 
+            _myAction1 += SendMsg1;
+            _myAction1?.Invoke(5); // 為空更簡潔的寫法
+
+
+            // 帶入Action <string>
+            _myAction2 += SendMsg2;
+            _myAction2?.Invoke("我是XXX"); // 為空更簡潔的寫法
+
+            // 帶入Func <int,int>
+            _myAction3 += SendMsg3;
+            _myAction3?.Invoke(10); // 為空更簡潔的寫法
+
+            // 帶入Func <string , string >
+            _myAction4 += SendMsg4;
+            _myAction4?.Invoke("我是OOO"); // 為空更簡潔的寫法
+
+            // Lambda 表達式
+            _myAction5 = () =>
+            {
+                Console.WriteLine("Hello World Lambda");
+            };
+
+            _myAction5 = () => Console.WriteLine("Hello World Lambda 簡寫"); // 一行寫法
+
+            _mythDelegate = AddNumner;
+            int result = _mythDelegate(3, 5);
+            Console.WriteLine($"_mythDelegate : {result}");
+
+            _mythDelegate1 = (a,b) => a + b; // Lambda + Func 一行寫法簡寫(有回傳值) | 不用多寫 AddNumner 方法
+            int result2 = _mythDelegate1(7, 8);
+            Console.WriteLine($"_mythDelegate1 : {result2}");
+
+
         }
 
-        private void SendMsg()
+        private void SendMsg0()
         {
-            Console.WriteLine("Hello World");
+            Console.WriteLine($"Hello World");
         }
 
-        private void SendMsg2()
+
+        private void SendMsg1(int number)
         {
-            Console.WriteLine("Hello World2");
+            Console.WriteLine($"Hello World，這是數字{number}");
+        }
+
+        private void SendMsg2(string msg)
+        {
+            Console.WriteLine("Hello World2"+msg);
+        }
+
+        private int SendMsg3(int number)
+        {
+            return number;
+        }
+
+        private string SendMsg4(string msg)
+        {
+            return "Hello World2" + msg;
         }
 
         private void PrintSum()
@@ -38,6 +111,11 @@ namespace DelegateExample
                 sum += i;
                 Console.WriteLine($"數字:{i},總和:{sum}");
             }
-        }        
+        }
+
+        private int AddNumner(int a , int b)
+        {
+            return a + b;
+        }
     }
 }
--- C#/Delegate/DelegateExample/DelegateExample/Delegate2.cs
+++ C#/Delegate/DelegateExample/DelegateExample/Delegate2.cs
@@ -14,27 +14,60 @@ namespace DelegateExample
 
         public void Main()
         {
-            // 因每個既能固定都消耗魔力與播放特效，只要把不同的技能效果傳進來即可
+            #region 使用傳統方法
+            CastSkill1();
+            CastSkill2();
+            CastSkill3();
+            #endregion
+
+            #region 使用委派方法
+            // 因每個技能固定都消耗魔力與播放特效，只要把不同的技能效果傳進來即可
             CastSkill(DoDamage); 
             CastSkill(DoHeal); 
-            CastSkill(DoTelePort); 
+            CastSkill(DoTelePort);
+            #endregion
         }
 
+        #region 若技能一直增加，會導致方法Funtion數量爆炸
+        public void CastSkill1()
+        {
+            ConsumeMP();
+            PlayEffects();
+            DoDamage();
+        }
+
+        public void CastSkill2()
+        {
+            ConsumeMP();
+            PlayEffects();
+            DoHeal();
+        }
+
+        public void CastSkill3()
+        {
+            ConsumeMP();
+            PlayEffects();
+            DoTelePort();
+        }
+        // .... 一直增加技能，Funtion越來越多
+        // public void CastSkill4()
+        #endregion
+
+        #region 改成委派的方式
         public void CastSkill(SkillDelegate skill)
         {
             ConsumeMP();
             PlayEffects();
-            skill();
+            skill(); // 傳入不同的技能
         }
+        #endregion
 
         public void ConsumeMP() => Console.WriteLine("消耗魔力");
         public void PlayEffects() => Console.WriteLine("播放特效");
 
         // 技能施放後效果
         public void DoDamage() => Console.WriteLine("造成傷害");
-        public void DoHeal() => Console.WriteLine("造成傷害");
-        public void DoTelePort() => Console.WriteLine("造成傷害");
-
-    
+        public void DoHeal() => Console.WriteLine("治療回血");
+        public void DoTelePort() => Console.WriteLine("進行傳送");    
     }
 }
