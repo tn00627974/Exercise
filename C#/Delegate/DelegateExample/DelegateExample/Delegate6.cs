0e9b568f473c30b23cdd5bd474114f75e1b78379
tn00627974 <tn00627974@diyi.org.tw>
2025-09-26 10:29:57 +0800

    改成 LevelSystem 訂閱 Score 並傳遞 - 使用 LevelSystem 可呼叫當前等級。

--- C#/Delegate/DelegateExample/DelegateExample/Delegate6.cs
+++ C#/Delegate/DelegateExample/DelegateExample/Delegate6.cs
@@ -10,7 +10,24 @@ using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace DelegateExample6
 {
-    /*
+    /*  觀察者模式 (Observer Pattern)
+     *   定義一個一對多的關係，讓多個物件監聽某個主體物件的狀態變化，並自動更新相關物件的行為。
+     *   觀察者模式是一種物件導向的設計模式，其核心思想是：物件之間的關係由觀察者和被觀察者所組成，被觀察者在狀態變化時，會通知所有觀察者，觀察者再根據自己的需求來更新自己的行為。
+     *   觀察者模式的主要角色：
+     *   1. Subject (主體物件)：被觀察者，也就是被通知的物件。
+     *   2. Observer (觀察者)：監聽主體物件的物件。
+     *   3. ConcreteSubject (具象主體物件)：實作主體物件的具象類別。
+     *   4. ConcreteObserver (具象觀察者)：實作觀察者的具象類別。
+     *   5. Client (客戶端)：使用主體物件的類別。
+     *   觀察者模式的優點：
+     *   1. 降低耦合度：主體物件和觀察者之間的耦合度降低，可讓主體物件和觀察者之間的通信簡化。
+     *   2. 容易增加和刪除觀察者：增加或刪除觀察者只需要修改主體物件的狀態，而不需要修改觀察者的程式碼。
+     *   3. 容易封裝狀態：主體物件的狀態變化時，觀察者可以根據自己的需求來更新自己的行為，而不用知道主體物件的實際狀態。
+     *   觀察者模式的缺點：
+     *   1. 觀察者太多時，效率低下：當觀察者太多時，主體物件的狀態變化會通知所有觀察者，造成效率的降低。
+     *   2. 觀察者太多時，通知過多：當觀察者太多時，主體物件的狀態變化會通知所有觀察者，造成通知過多的問題。
+     *   3. 觀察者沒有對應的主體物件：當觀察者沒有對應的主體物件時，無法監聽到主體物件的狀態變化。
+     *   4. 觀察者的通知順序不固定：當主體物件的狀態變化時，觀察者的通知順序不固定，造成觀察者的行為不一致。
      * 📌 題目描述
 
         請你實作一個簡易的 GameManager，他會觸發兩種事件：
@@ -67,15 +84,13 @@ namespace DelegateExample6
             Console.WriteLine($"玩家受傷 {damage}");
             OnPlayerDamaged?.Invoke(damage);
         }
-        public void PlayerLevelUp(int level)
-        { }
     }
 
     public class ScoreSystem : IDisposable
     {
         private GameManager _gameManager;
         public int _score;
-        private Action<int> LvScore;
+        public event Action<int> OnScoreChanged;
         public ScoreSystem(GameManager gameManager)
         {
             _gameManager = gameManager;
@@ -85,6 +100,7 @@ namespace DelegateExample6
         {
             _score += point;
             Console.WriteLine($"玩家目前得分 : {_score}");
+            OnScoreChanged?.Invoke(_score); 
         }
 
         public void Dispose()
@@ -143,31 +159,36 @@ namespace DelegateExample6
         }
     }
 
+
     public class LevelSystem : IDisposable
     {
-        private GameManager _gameManager;
         private ScoreSystem _scoreSystem;
         private int _level;
-        public LevelSystem(GameManager gameManager ,ScoreSystem scoreSystem)
+        public int level => _level; // 新增屬性，取得等級
+        public Action<int> OnLevelChanged;
+        public LevelSystem(ScoreSystem scoreSystem)
         {
-            _gameManager = gameManager;
             _scoreSystem = scoreSystem;
-            _gameManager.OnPlayScrored += GetLevelScored;
+            _scoreSystem.OnScoreChanged += GetLevelScored;
             _level = 1;
-            int point = _scoreSystem._score;
-
         }
-        public void GetLevelScored(int point)
+        public void GetLevelScored(int score)
         {
-            if (_scoreSystem._score >= 50)
+            if (score >= 50)
             {
                 _level++;
                 Console.WriteLine($"玩家升級，等級 {_level}");
+                OnLevelChanged?.Invoke(_level);
             }
+        }
+        //取得等級
+        public void PlayerLevel()
+        {
+            Console.WriteLine($"玩家等級 {level}");
         }
         public void Dispose()
         {
-            _gameManager.OnPlayScrored -= GetLevelScored;
+            //_scoreSystem.OnScoreChanged -= GetLevelScored;
             Console.WriteLine("LevelSystem 已釋放");
         }
     }
@@ -178,14 +199,16 @@ namespace DelegateExample6
         GameManager gm = new GameManager();
         public void Main()
         {
-            using (var ss = new ScoreSystem(gm))          
             using (var ui = new UIManger(gm))
+            using (var ss = new ScoreSystem(gm))
             using (var hs = new HealthSystem(gm))
-            using (var ls = new LevelSystem(gm, ss))
+            using (var ls = new LevelSystem(ss))
             {
+                ls.OnLevelChanged += (level) => Console.WriteLine($"玩家等級: {level}");
+
                 gm.StartGame();
                 gm.PlayerScored(30);
-                gm.PlayerScored(40);
+                gm.PlayerScored(20);
                 gm.PlayerScored(50);
                 gm.PlayerDamaged(90);
                 gm.PlayerDamaged(40);
