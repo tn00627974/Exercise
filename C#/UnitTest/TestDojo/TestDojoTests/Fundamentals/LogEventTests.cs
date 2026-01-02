eeac1c9f21bff8511473c299a07eb1e5d0552b92
DESKTOP-TM1N0UT\Ulysses <tn00627974@gmail.com>
2026-01-02 21:34:16 +0800

    新增測試:
    TestDojoTests.csproj：新增對 Microsoft.Extensions.Logging.Abstractions 的引用以支持日誌記錄功能
    FizzBuzzTests.cs：新增對結果陣列的多個斷言以檢查 FizzBuzz 的輸出結果，並移除不必要的結束大括號
    LearYearTests.cs：更改方法名稱以提高可讀性，並新增測試方法以檢查年份 2000 是否為閏年
    FormatterTests.cs：新增測試類及測試方法以驗證 Formatter 類的 FormatBold 方法是否正確包裝文本
    LogEventTests.cs：新增測試類及測試方法以測試 LogEvent 類的日誌記錄功能，確保事件觸發時能正確記錄錯誤信息

---
+++ C#/UnitTest/TestDojo/TestDojoTests/Fundamentals/LogEventTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging.Abstractions;
+using TestDojo.Fundamentals;
+
+namespace TestDojoTests.Fundamentals
+{
+    [TestFixture]
+    public class LogEventTests
+    {
+        [Test]
+        public void LogEvent_When_EventTrigger()
+        {
+            var logger = new LogEvent(NullLogger.Instance);
+            logger.LogError("tesing");
+
+            Guid myGuid = Guid.Empty;
+            logger.ErrorLogged += (sender, guid) => { myGuid = guid; };
+
+            Assert.That(myGuid , Is.EqualTo(Guid.Empty));
+        }
+    }
+
+    
+}
