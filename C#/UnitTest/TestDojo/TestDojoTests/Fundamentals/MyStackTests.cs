ab702445e6cec38f162fa8e2ae287bc0dc759b42
DESKTOP-TM1N0UT\Ulysses <tn00627974@gmail.com>
2026-01-09 22:30:24 +0800

    調整測試類別名稱並新增 MyStack 單元測試
    - LearYearTests 新增 2000 年閏年測試
    - 修正 NumberTests 中 Assert.AreEqual 格式
    - 新增 MyStackTests，涵蓋 Push、Pop、Peek 等行為

---
+++ C#/UnitTest/TestDojo/TestDojoTests/Fundamentals/MyStackTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestDojo.Fundamentals;
+
+namespace TestDojoTests.Fundamentals
+{
+    [TestFixture]
+    internal class MyStackTests
+    {
+        [Test]
+        public void Count_WhenNoEmpty_ReturnCorrectCount()
+        {
+            // 3A
+            // Arrange
+            var myStack = new MyStack<int>();
+
+            // Act
+            myStack.Push(1);
+            myStack.Push(2);
+            myStack.Push(3);
+
+            // Assert
+            Assert.That(myStack.Count == 3);
+        }
+
+        [Test]
+        public void Push_WhenNoEmpty_ReturnCorrectValue()
+        { 
+            var myStack = new MyStack<int>();
+            myStack.Push(1);
+            myStack.Push(2);
+            myStack.Push(3);
+            Assert.That(myStack.Count == 3);
+        }
+
+        [Test]
+        public void Pop_WhenNoEmpty_ReturnCorrectValue()
+        {
+            var myStack = new MyStack<int>();
+            myStack.Push(1);
+            myStack.Push(2);
+            myStack.Push(3);
+            var result = myStack.Pop();
+            Assert.That(result == 3);
+            Assert.That(myStack.Count == 2);
+        }
+
+        [Test]
+        public void Peek_WhenNoEmpty_ReturnCorrectValue()
+        {
+            var myStack = new MyStack<int>();
+            myStack.Push(1);
+            myStack.Push(2);
+            var result = myStack.Peek();
+            Assert.That(result == 2);
+            Assert.That(myStack.Count == 2);
+        }
+
+    }
+}
